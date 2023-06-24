86a77f72cf4afa343112658b77d02ceb9d124a6b
18107 <18107@users.noreply.github.com>
2023-06-24 19:19:14 +0930

    Add files via upload

---
+++ Mod.cs
@@ -0,0 +1,22 @@
+﻿using PulsarModLoader;
+
+namespace Win_by_default
+{
+    public class Mod : PulsarMod
+    {
+        public override string Version => "0.0.0";
+
+        public override string Author => "18107";
+
+        public override string ShortDescription => "You win the race immediately if all other ships have been destroyed.";
+
+        public override string Name => "Win by default";
+
+        public override string ModID => "winbydefault";
+
+        public override string HarmonyIdentifier()
+        {
+            return "id107.winbydefault";
+        }
+    }
+}
---
+++ Race.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+
+namespace Win_by_default
+{
+    [HarmonyPatch(typeof(PLRace), "Update")]
+    internal class Race
+    {
+        static void Prefix(PLRace __instance, PLRaceEncounterBase ___MyRacingEncounter)
+        {
+            if (__instance.InProgress && PLNetworkManager.Instance.LocalPlayer.GetPhotonPlayer().IsMasterClient && ___MyRacingEncounter != null && ___MyRacingEncounter.GetRacers() != null)
+            {
+                int opponentCount = 0;
+                foreach (PLShipInfoBase ship in ___MyRacingEncounter?.GetRacers())
+                {
+                    if (ship != null && ship != PLEncounterManager.Instance.PlayerShip)
+                    {
+                        opponentCount++;
+                    }
+                }
+                if (opponentCount == 0)
+                {
+                    PLEncounterManager.Instance.PlayerShip.CompletedLaps = PLEncounterManager.Instance.PlayerShip.CurrentRace.Laps;
+                }
+            }
+        }
+    }
+}
